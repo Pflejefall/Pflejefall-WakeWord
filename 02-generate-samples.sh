0fa8f538743f11b3124e4beb9f35558c44160836
Chris Ribal <c.ribal@ribal-webentwicklung.de>
2026-01-29 21:13:21 +0100

    Update sample generation parameters and query string

--- 02-generate-samples.sh
+++ 02-generate-samples.sh
@@ -1,7 +1,7 @@
 python3 \
     ./piper-sample-generator/generate_samples.py \
-    'pflejefall.' \
+    'pfleyefall?' \
     --model piper_voices/de_DE-thorsten-medium.onnx \
-    --max-samples 500 \
+    --max-samples 1000 \
     --batch-size 10 \
     --output-dir generated_samples/
